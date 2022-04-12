892c600d9e637e266a37e51a388605a2251de466
Aleksandr Bushuev <Aleksandr.Bushuev.97@mail.ru>
2022-04-12 23:37:04 +0500

    Создал поставщика подключения к базе sqlite

---
+++ AndroidMonitoring/AndroidMonitoring/Repositories/DataBaseProvider.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AndroidMonitoring.Repositories
+{
+    public static class DataBaseProvider
+    {
+        public static SQLiteConnection GetSQLiteConnection()
+        {
+            string dbPath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "monitoring.db3");
+            var db = new SQLiteConnection(dbPath);
+            return db;
+        }
+
+    }
+}
