023f4febb861f3ccac4af8d1c021afbc714cadfb
Aleksandr Bushuev <Aleksandr.Bushuev.97@mail.ru>
2022-04-12 23:39:30 +0500

    Добавил представление и активити для графика

---
+++ AndroidMonitoring/AndroidMonitoring/GraphActivity.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AndroidMonitoring
+{
+    [Activity(Label = "GraphActivity")]
+    public class GraphActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.layout_graph);
+
+          
+        }
+    }
+}
